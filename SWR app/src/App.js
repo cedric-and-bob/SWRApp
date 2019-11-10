06970bed2285e43a6a1a09c3fb6c24e1b4475931
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-09 23:57:53 -0700

    Restrucutred the App.js file to look and act more ReactIsh

--- SWR app/src/App.js
+++ SWR app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { Header } from "./components/Header/index";
 
 import "./styles/styles.css";
 
@@ -8,32 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className="headerContainer">
-        <header className="header">
-          <Link to="/weather" className="link">
-            Weather
-          </Link>
-          <Link to="/Traffic" className="link">
-            Traffic
-            <sup>
-              {" "}
-              <em>Coming Soon</em>
-            </sup>
-          </Link>
-          <Link to="/Gas" className="link">
-            Gas
-            <sup>
-              {" "}
-              <em>Coming Soon</em>
-            </sup>
-          </Link>
-          <Link to="/Food" className="link">
-            Food
-            <sup>
-              {" "}
-              <em>Coming Soon</em>
-            </sup>
-          </Link>
-        </header>
+        <Header />
       </div>
     </div>
   );
