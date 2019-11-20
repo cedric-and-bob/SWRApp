f9f640872652b08b74d2eca90311f5b5bb233c2c
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-20 12:49:05 -0700

    Changed the console.log

--- SWRapp/src/components/WeatherBody/index.js
+++ SWRapp/src/components/WeatherBody/index.js
@@ -17,7 +17,7 @@ function WeatherBody() {
 
   if (error) return <div>{console.log(error)}</div>;
   if (!data) return <div>loading...</div>;
-  console.log({ data });
+  console.log(data);
   return (
     <div>
       {/* {<Name />
