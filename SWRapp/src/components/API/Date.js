f819ac58fc14db7c01eacd3c5b26ca38d94608b0
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-20 14:28:10 -0700

    Finally the three pieces of data are being retrieved and displayed correctly on the browser page

--- SWRapp/src/components/API/Date.js
+++ SWRapp/src/components/API/Date.js
@@ -3,18 +3,17 @@ import React from "react";
 class Date extends React.Component {
   render() {
     let options = {
-      weekday: "long",
-      year: "numeric",
+      // weekday: "long",
+      // year: "numeric",
       month: "long",
       day: "numeric"
     };
-    var date = new Date();
 
-    let actualToday = date.DateTimeFormat("en-US", options);
+    var date = new Intl.DateTimeFormat("en-US", options).format(date);
 
     return (
       <div>
-        <div>{actualToday}</div>
+        <div>Today, {date}</div>
       </div>
     );
   }
