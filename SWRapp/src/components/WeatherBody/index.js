f9d9bc07b015ae005a7eaf96215b4c3d3730a5d6
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-20 12:45:32 -0700

    Added the cors url

--- SWRapp/src/components/WeatherBody/index.js
+++ SWRapp/src/components/WeatherBody/index.js
@@ -11,7 +11,7 @@ const fetcher = url => fetch(url).then(r => r.json());
 
 function WeatherBody() {
   const { data, error } = useSWR(
-    "https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22",
+    "https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22",
     fetcher
   );
 
