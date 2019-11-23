3c931e60a295840749bf40337719ba5ea1ae8225
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-23 00:05:25 -0700

    Added a views folder to more easily handle the router aspect of this CRA project

---
+++ SWRapp/src/views/index.js
@@ -0,0 +1,6 @@
+export { default as WeatherPageView } from "./WeatherPageView";
+// export { default as LandingPageView } from "./LandingPageView";
+// export { default as WeatherPageViewAlt } from "./WeatherPageViewAlt";
+// export { default as TrafficPage } from "./TrafficPageView";
+// export { default as GasPage } from "./GasPageView";
+// export { default as FoodPage } from "./FoodPage";
