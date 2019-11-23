71bc6f14b09007caba7d9bc1cb95740ec1021100
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-23 15:22:17 -0700

    Modified the props received and the values located within the page

--- SWRapp/src/components/AltWeather/Stations.js
+++ SWRapp/src/components/AltWeather/Stations.js
@@ -3,18 +3,15 @@ import React, { Component } from "react";
 import Location from "./Location";
 import Temp from "./Temp";
 
-import { useParams } from "react-router-dom";
-
 class Stations extends Component {
   render() {
-    let { stationId } = useParams();
-    const { city, state, temp } = this.props;
+    console.log("this.props.data", this.props.data);
+    const { city, state, observation } = this.props.data;
 
     return (
       <div className="stations">
         <Location city={city} state={state} />
-        <Temp temp={temp} />
-        {stationId}
+        <Temp temp={observation[0].temperature} />
       </div>
     );
   }
