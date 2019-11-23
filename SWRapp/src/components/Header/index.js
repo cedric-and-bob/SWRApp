3c0a330854aabb749e26be7f30f5b5fc7b122955
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-23 00:04:57 -0700

    Modified the header component with viable links and text to make the header more apt with future versions of the app

--- SWRapp/src/components/Header/index.js
+++ SWRapp/src/components/Header/index.js
@@ -7,24 +7,20 @@ import "../../styles/styles.css";
 export const Header = props => {
   return (
     <header className="header">
+      <Link to="/" className="link">
+        Home
+      </Link>
       <Link to="/weather" className="link">
         Weather
       </Link>
-      <Link to="/Traffic" className="link">
+      <Link to="/traffic" className="link">
         Traffic
         <sup>
           {" "}
           <em>Coming Soon</em>
         </sup>
       </Link>
-      <Link to="/Gas" className="link">
-        Gas
-        <sup>
-          {" "}
-          <em>Coming Soon</em>
-        </sup>
-      </Link>
-      <Link to="/Food" className="link">
+      <Link to="/good" className="link">
         Food
         <sup>
           {" "}
